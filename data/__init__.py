9cda8f876df780f3fac618e1111a18f47865d670
Arun Prabhu <arun.prabhu@mail.inf.h-brs.de>
2021-09-01 08:48:42 +0200

    Feature: Enabling access to blender temporal dataset

--- data/__init__.py
+++ data/__init__.py
@@ -2,8 +2,10 @@ from .llff import LLFFDataset
 from .blender import BlenderDataset
 from .dtu_ft import DTU_ft
 from .dtu import MVSDatasetDTU
+from .blender_temporal import BlenderTemporalDataset
 
 dataset_dict = {'dtu': MVSDatasetDTU,
                 'llff':LLFFDataset,
                 'blender': BlenderDataset,
+                'blender_temporal': BlenderTemporalDataset,
                 'dtu_ft': DTU_ft}
